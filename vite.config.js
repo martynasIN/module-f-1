731207d9b11c4d677df273ddaa245b15e378293a
Martynas <m.kaselionis@gmail.com>
2025-03-03 10:14:25 +0200

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-template/", // Pakeisk pagal savo repo pavadinimą
+  base: "/module-f-1/", // Pakeisk pagal savo repo pavadinimą
 });
