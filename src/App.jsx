aff9a054c5803127327a6c76514e64d37dc081c8
Martynas <m.kaselionis@gmail.com>
2025-03-03 10:22:20 +0200

    Change vite config

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React for competition WEB dev challenge 2025</h1>
+        <h2>Competitor: 2</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
